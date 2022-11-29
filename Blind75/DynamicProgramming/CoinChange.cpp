cce5cbfa05217d5d11abd932c8a1cba136e9ac0e
anidub <anish_dubey@hotmail.com>
2022-11-28 19:27:44 -0500

    Update CoinChange.cpp

--- Blind75/DynamicProgramming/CoinChange.cpp
+++ Blind75/DynamicProgramming/CoinChange.cpp
@@ -40,6 +40,57 @@ public:
 
 		return dp[amount] > amount ? -1 : dp[amount];
 	}
+	
+	
+	
+	    //Recursion:Time: O(2^n) Space: O(n)
+    int coinChangeBrute(vector<int>& coins, int amount) { 
+        int res = findLowestCoinsBrute(coins, 0, amount);        
+        return (res == INT_MAX - 1)? -1 : res;
+    }
+    
+    int findLowestCoinsBrute(vector<int>& coins, int cur, int amount) {
+        if(cur >= coins.size() || amount <= 0) {
+            return (amount == 0) ? 0 : INT_MAX - 1;
+        }
+        int res = -1;
+        if(coins[cur] > amount) {
+            int doNotTakeCoin = 0 + findLowestCoinsBrute(coins, cur + 1, amount - 0);
+            res = doNotTakeCoin;
+        } else {
+            int takeCoin = 1 + findLowestCoinsBrute(coins, cur + 0, amount - coins[cur]);
+            int doNotTakeCoin = 0 + findLowestCoinsBrute(coins, cur + 1, amount - 0);
+            res = min(takeCoin, doNotTakeCoin);
+        }
+        return res;
+    }
+    
+    
+    //Memoization Time: O(n.m) Space: O(n.m)
+    int memo[12 + 1][10000 + 1];
+    int coinChangeMemo(vector<int>& coins, int amount) {
+        memset(memo, -1, sizeof(memo));
+        int res = findLowestCoinsMemo(coins, 0, amount);
+        return (res == INT_MAX - 1) ? -1 : res;
+    }
+    
+    int findLowestCoinsMemo(vector<int>& coins, int cur, int amount) {
+        if(cur >= coins.size() || amount <= 0) {
+            return amount == 0 ? 0 : INT_MAX - 1;
+        }
+        if(memo[cur][amount] != -1) return memo[cur][amount];
+        int res = -1;
+        if(coins[cur] > amount) {
+            int doNotTakeCoin = 0 + findLowestCoinsMemo(coins, cur + 1, amount - 0);
+            memo[cur][amount] = res = doNotTakeCoin;
+        } else {
+            int takeCoin = 1 + findLowestCoinsMemo(coins, cur + 0, amount - coins[cur]);
+            int doNotTakeCoin = 0 + findLowestCoinsMemo(coins, cur + 1, amount - 0);
+            res = min(takeCoin, doNotTakeCoin);
+            memo[cur][amount] = res;
+        }
+        return memo[cur][amount];
+    }
 };
 
 /*
